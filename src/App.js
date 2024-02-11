58b3160953d2d340b3a4d0b9afa698658d518546
Antonio Virgone <antonio.virgone@wisemotions.com>
2024-02-11 18:59:00 +0100

    create new component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import FiglioWithoutStatus from './FiglioWithoutStatus';
-import FiglioWithStatus from './FiglioWithStatus'
 import Stock from './Stock';
 
 function App() {
+  const stockList = [
+    {name: 'appl', founder: 'Jobs'},
+    {name: 'GOOG', founder: 'Brin'},
+    {name: 'AMZN', founder: 'Bezos'}
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <FiglioWithStatus />
-        <FiglioWithoutStatus />
-        <table>
-        <tbody>
-          <tr>
-            <Stock />
-          </tr>
-          </tbody>
-        </table>
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 32
-        </a>
+        { stockList.map(item => <Stock name={item.name.toUpperCase()} founder={item.founder} /> ) }       
       </header>
     </div>
   );
--- src/Stock.js
+++ src/Stock.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import FiglioStock from './FiglioStock';
+import RigaHobby from './RigaHobby';
 
 class Stock extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.hobby = ['element1','element2','element3'];
+    }
+    
+
   render() {
+    const { name, founder } = this.props;
     return (
-      <React.Fragment>
-        <FiglioStock name="APPLE" />
-        <FiglioStock age="18"/>
-      </React.Fragment>
+      <div>
+        <h3>Figlio: {name}</h3>
+        <p>Fondatore: {founder}</p>
+        <p>Lista Hobby</p>
+        <ul>{ this.hobby.map(item => <RigaHobby item={item} />)}</ul>
+      </div>
     )
   }
 }
